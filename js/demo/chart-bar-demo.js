03b41ccc387f413b189bef0ac8deae1080ab5c94
Ching Ching <hoichingtong@marriottschool.byu.edu>
2019-11-06 13:29:15 -0700

    Finished charts

--- js/demo/chart-bar-demo.js
+++ js/demo/chart-bar-demo.js
@@ -29,17 +29,34 @@ function number_format(number, decimals, dec_point, thousands_sep) {
 
 // Bar Chart Example
 var ctx = document.getElementById("myBarChart");
+
+data1 = Array(12).fill(1).map(x => get_random(50,80))
+data2 = Array(12).fill(1).map(x => get_random(50,80))
+
+console.log("Ching's Charts");
+console.log(data1);
+console.log(data2);
+
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
     labels: ["January", "February", "March", "April", "May", "June"],
-    datasets: [{
-      label: "Revenue",
-      backgroundColor: "#4e73df",
+    datasets: [
+    {
+      label: "Expected Sales",
+      backgroundColor: "#7E9181",
       hoverBackgroundColor: "#2e59d9",
-      borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
-    }],
+      borderColor: "#7E9181",
+      data: data1,
+    },
+    {
+      label: "Actual Sales",
+      backgroundColor: "#9BC1BC",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#9BC1BC",
+      data: data2,
+    },
+  ],
   },
   options: {
     maintainAspectRatio: false,
@@ -51,61 +68,8 @@ var myBarChart = new Chart(ctx, {
         bottom: 0
       }
     },
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'month'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          callback: function(value, index, values) {
-            return '$' + number_format(value);
-          }
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
     legend: {
       display: false
-    },
-    tooltips: {
-      titleMarginBottom: 10,
-      titleFontColor: '#6e707e',
-      titleFontSize: 14,
-      backgroundColor: "rgb(255,255,255)",
-      bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
-      borderWidth: 1,
-      xPadding: 15,
-      yPadding: 15,
-      displayColors: false,
-      caretPadding: 10,
-      callbacks: {
-        label: function(tooltipItem, chart) {
-          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
-        }
-      }
     },
   }
 });
