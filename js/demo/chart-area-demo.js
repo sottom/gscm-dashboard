7303117640e58ab92690011d1e1b4c4574e95942
Mitchell Sotto <sottochoro@gmail.com>
2019-11-11 11:44:43 -0700

    bob

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -191,7 +191,7 @@ var myLineChart = new Chart(demand, {
           padding: 10,
           // Include a dollar sign in the ticks
           callback: function(value, index, values) {
-            return '$' + number_format(value);
+            return number_format(value);
           }
         },
         gridLines: {
@@ -223,7 +223,7 @@ var myLineChart = new Chart(demand, {
       callbacks: {
         label: function(tooltipItem, chart) {
           var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-          return datasetLabel + ': $' + number_format(tooltipItem.yLabel);
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel);
         }
       }
     }
